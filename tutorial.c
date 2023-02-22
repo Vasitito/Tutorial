9a068543c08a227436a9337345a22a73e96cda7c
Vasitito <vasitito@DESKTOP-R5NV571.localdomain>
2023-02-22 21:32:20 +0200

    update

--- tutorial.c
+++ tutorial.c
@@ -1,23 +1,22 @@
 #include "function.h"
 
-int main(int argc, char* argv[])
-{
-    int variable_a,variable_b,temp;
+int fact(int n);
 
+int main(int argc, char* argv[]){
+    int variable_a,variable_b;
+    int final;
     variable_a=75;
-    variable_b=32;
-    
+    variable_b=fact(9);;
+        
     for(int i=0;i<4;i++){
         if(variable_a<variable_b){
             variable_b=variable_b-10;
-        }
-        else{
+        }else{
             variable_a=variable_a-10;
         }
     }
 
-    temp=max(variable_a,variable_b);
+    final=max(variable_a,variable_b);
 
-
-    return 0;
+    return final;
 }
